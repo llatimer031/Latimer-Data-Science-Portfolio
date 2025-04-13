61ec72125a8d0bdc2a308f66521fe0e3cade6fa9
llatimer031 <130508068+llatimer031@users.noreply.github.com>
2025-04-13 15:31:30 -0400

    Restructure variable selection

--- MLStreamlitApp/Main.py
+++ MLStreamlitApp/Main.py
@@ -2,24 +2,48 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
 
 # ---------- TITLE ---------- #
 # set up title, author, and description
 st.set_page_config(layout='centered')
 st.title("Supervised Learning via Streamlit:")
-st.header("A Visual Approach to Model Selection and Tuning")
+st.header("An interactive walkthrough data processing, model selection, and parameter tuning for classification models.")
 st.write("By Lauren Latimer | Access code on [Github](https://github.com/llatimer031/Latimer-Data-Science-Portfolio/tree/main/MLStreamlitApp)")
 
 # ---------- FUNCTIONS ---------- #
 
-def LogRegression():
-    pass
+def LogRegression(X_train, y_train):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
 
-def ClassTree():
-    pass
+def ClassTree(X_train, y_train):
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    return model
 
-def kNNClassifier():
-    pass
+def kNNClassifier(k, X_train, y_train):
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train, y_train)
+    return model
+
+def ConfMatrix(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy}")
+    
+    cm = confusion_matrix(y_test, y_pred)
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.show()
+    
 
 # ---------- SIDEBAR ---------- #
 # create sidebar to customize data and model options
@@ -50,6 +74,7 @@ else:
 # ---------- MAIN ---------- #
 st.header("Part 1: Processing the Data")
 
+data_ready = False
 if df is not None:
     # preview the chosen dataset 
     st.write("Below is a preview of your chosen dataset:")
@@ -75,27 +100,62 @@ if df is not None:
         st.success("Selected columns encoded using one-hot encoding.")
     else:
         st.warning("No columns selected for encoding.")
+        
+    data_ready = True
 
-    # specify x and y columns
-    st.subheader("Step 3: Choose the Target Variable")
-    label = st.selectbox("Select the target column (y):", df.columns)
-    if label:
-        X = df.drop(label, axis=1)
-        y = df[label]
-        st.write(f"You have chosen the {label} variable to be your label.")
 
-    # split into training and test data
-    st.subheader("Step 4: Test Train Split")
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    
-    st.header("Part 2: Train a Supervised Learning Model")
+st.header("Part 2: Model and Variable Selection") 
+
+data_split = False
+if data_ready:
     # choose a model to train
+    st.subheader("Step 1: Choose a Classification Model")
     choice = st.selectbox('Select Model Type', ['Logistic Regression', 'Classification Tree', 'kNN'])
 
-    if choice == 'Logistic Regression':
-        LogRegression()
-    elif choice == 'Classification Tree':
-        ClassTree()
+    # specify x and y columns
+    st.subheader("Step 2: Choose Features and Target Variable")
+    
+    st.write("For this app, please select a target variable that is binary categorical (ex: Yes/No, True/False, 0/1).")
+    label = st.selectbox("Select the target column (y):", df.columns)
+    st.write(f"You have chosen the **{label}** variable to be your label.")
+    y = df[label]
+    st.dataframe(y.head())
+
+    st.write("For this app, please select features that are numeric or have already been encoded.")
+    # limit feature selection to numeric or encoded columns
+    numeric_columns = df.select_dtypes(include=['int64', 'float64', 'int32', 'float32']).columns.tolist()
+    # do not allow label to be chosen as feature
+    if label in numeric_columns:
+        numeric_columns.remove(label)
+        
+    features = st.multiselect("Select the feature columns (X):", numeric_columns)
+    if features:
+        st.write(f"You have chosen the following features: **{features}**")
+        X = df[features]
+        st.dataframe(X.head())
     else:
-        kNNClassifier()
+        st.warning("Please select at least one feature.")
+    
+    # split into training and test data
+    st.subheader("Step 3: Test Train Split")
+    if 'X' in locals() and 'y' in locals(): # do not execute split until x and y have been chosen
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        st.success("The data has been split into 80% training data and 20% test data.")
+        data_split = True
+    else:
+        st.warning("Variable selection must be complete to continue.")
+else:
+    st.warning("Data is not ready for this step.")
+    
+st.header("Part 3: Run the Classification Model")
+if data_split:
+    # run the models
+    if choice == 'Logistic Regression':
+        LogRegression(X_train, y_train)
+    elif choice == 'Classification Tree':
+        ClassTree(X_train, y_train)
+    else:
+        k = st.slider("Select a k-value:", 1, 10)
+        kNNClassifier(k, X_train, y_train)
+else:
+    st.warning("Data is not ready for this step.")
